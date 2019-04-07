53bf6bc05bcc1bd964e94a7ee43b9d41fd1047cf
Lev-Stambler <lev@britesignal.com>
2019-04-07 06:23:37 -0400

    alot

---
+++ index.js
@@ -0,0 +1,160 @@
+const elems = document.getElementsByClassName('piece')
+console.log(elems)
+
+let playerMove = 'p1'
+
+for (const piece of elems) {
+  console.log(piece)
+  piece.addEventListener('dragstart', dragstart)
+  piece.addEventListener('dragover', dragover)
+  piece.addEventListener('dragenter', dragenter)
+  piece.addEventListener('drop', (event) => {
+    drop(piece.getAttribute('index'), event, piece, piece.getAttribute('class').indexOf('p1') !== -1 ? 'p1' : 'p2')
+  })
+}
+
+const grids = document.getElementsByTagName('td')
+for (const td of grids) {
+  td.addEventListener('dragover', dragover)
+  td.addEventListener('drop', (event) => {
+    dropTD(null, event, td, 'p1')
+  })
+}
+
+function dragstart(event) {
+
+}
+
+function dragover(event) {
+  event.preventDefault()
+}
+
+function dragenter(event) {
+
+}
+
+function dropTD(info, event, elem, player) {
+  const oppPlayer = playerMove === 'p2' ? 'p1' : 'p2'
+  console.log('oppPlayer', oppPlayer)
+  event.preventDefault()
+  var data = event.dataTransfer.getData('text')
+  console.log(elem)
+  let appender
+  if (data.indexOf('rook') !== -1) {
+    appender = (document.querySelector(`.${oppPlayer}.rook`))
+  } else if (data.indexOf('pawn') !== -1) {
+    appender = (document.querySelector(`.${oppPlayer}.pawn`))
+  } else if (data.indexOf('king') !== -1) {
+    appender = (document.querySelector(`.${oppPlayer}.king`))
+  } else if (data.indexOf('diagonal') !== -1) {
+    appender = (document.querySelector(`.${oppPlayer}.diagonal`))
+  }
+
+  const appenderParentNumb = parseInt(appender.parentElement.getAttribute('class') ||
+    appender.parentElement.getAttribute('class')[0], 16)
+  console.log("appenderParentNumb", appenderParentNumb)
+
+  const hoverParentNumb = parseInt(elem.getAttribute('class') ||
+    elem.getAttribute('class')[0], 16)
+  console.log("hoverParentNumb", hoverParentNumb);
+
+  if (!isLegalMove(appender.getAttribute('index'), {
+      x: (appenderParentNumb - 1) % 3,
+      y: parseInt((appenderParentNumb - 1) / 3)
+    }, {
+      x: (hoverParentNumb - 1) % 3,
+      y: parseInt((hoverParentNumb - 1) / 3)
+    }, {
+      pawnMult: oppPlayer === 'p2' ? -1 : 1
+    })) {
+    alert('ILLEGAL MOVe2')
+    return false
+  }
+
+  const td = document.createElement('td')
+  elem.children[0].classList.remove(player)
+  elem.children[0].classList.add(oppPlayer)
+  if (elem.innerHTML.length > 2) {
+    td.innerHTML = elem.innerHTML
+    const posClass = oppPlayer !== 'p1' ? 'player1Pos' : 'player2Pos'
+    console.log(posClass)
+    document.querySelector(`.${posClass} table tr`).append(td)
+  }
+  elem.innerHTML = appender.outerHTML
+
+  // elem.appendChild(appender)
+  elem.removeEventListener('drop', () => {})
+
+  elem.addEventListener('drop', (event) => {
+    drop(appender.getAttribute('index'), event, appender, appender.getAttribute('class').indexOf('p1') !== -1 ? 'p1' : 'p2')
+  })
+  appender.remove()
+  playerMove = oppPlayer
+}
+
+function drop(info, event, elem, player) {
+  const oppPlayer = player === 'p2' ? 'p1' : 'p2'
+  console.log('oppPlayer', oppPlayer)
+  event.preventDefault()
+  var data = event.dataTransfer.getData('text')
+  console.log(elem)
+  let appender
+  if (data.indexOf('rook') !== -1) {
+    appender = (document.querySelector(`.${oppPlayer}.rook`))
+  } else if (data.indexOf('pawn') !== -1) {
+    appender = (document.querySelector(`.${oppPlayer}.pawn`))
+  } else if (data.indexOf('king') !== -1) {
+    appender = (document.querySelector(`.${oppPlayer}.king`))
+  } else if (data.indexOf('diagonal') !== -1) {
+    appender = (document.querySelector(`.${oppPlayer}.diagonal`))
+  }
+  const appenderParentNumb = parseInt(appender.parentElement.getAttribute('class') || appender.parentElement.getAttribute('class')[0], 16)
+
+  const hoverParentNumb = parseInt(elem.parentElement.getAttribute('class') ||
+    elem.parentElement.getAttribute('class')[0], 16)
+  if (!isLegalMove(appender.getAttribute('index'), {
+      x: (appenderParentNumb - 1) % 3,
+      y: parseInt((appenderParentNumb - 1) / 3)
+    }, {
+      x: (hoverParentNumb - 1) % 3,
+      y: parseInt((hoverParentNumb - 1) / 3)
+    }, {
+      pawnMult: oppPlayer === 'p2' ? -1 : 1
+    })) {
+    alert('ILLEGAL MOVe')
+    return false
+  }
+  const td = document.createElement('td')
+  elem.classList.remove(player)
+  elem.classList.add(oppPlayer)
+  td.innerHTML = elem.outerHTML
+  const posClass = oppPlayer !== 'p1' ? 'player1Pos' : 'player2Pos'
+  console.log(posClass)
+
+  document.querySelector(`.${posClass} table tr`).append(td)
+  elem.outerHTML = appender.outerHTML
+  // elem.appendChild(appender)
+  elem.removeEventListener('drop', () => {
+    elem.addEventListener('drop', (event) => {
+      drop(appender.getAttribute('index'), event, appender, appender.getAttribute('class').indexOf('p1') !== -1 ? 'p1' : 'p2')
+    })
+  })
+  appender.remove()
+}
+
+function isLegalMove(pieceIndex, currPos, futurePos, options) {
+
+  console.log(pieceIndex, currPos, futurePos)
+  if (parseInt(pieceIndex) == 0) { //diaganol
+    return Math.abs(currPos.x - futurePos.x) === 1 && Math.abs(currPos.y - futurePos.y) === 1
+  } else if (parseInt(pieceIndex) == 1) { //king
+    return Math.abs(currPos.x - futurePos.x) <= 1 && Math.abs(currPos.y - futurePos.y) <= 1 &&
+      !(Math.abs(currPos.x - futurePos.x) === 0 && Math.abs(currPos.y - futurePos.y) === 0)
+  } else if (parseInt(pieceIndex) === 2) { //rook
+    return (Math.abs(currPos.x - futurePos.x) === 1 || Math.abs(currPos.y - futurePos.y) === 1) &&
+      (Math.abs(currPos.x - futurePos.x) === 0 || Math.abs(currPos.y - futurePos.y) === 0)
+  } else if (parseInt(pieceIndex) == 3) { //pawn
+    return currPos.y - futurePos.y === 1 * options.pawnMult
+  }
+  // return true
+}
